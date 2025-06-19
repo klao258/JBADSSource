00b5ae75ea22e910ab9be7415d31df63cfe8da6e
klao258 <klao25388@gmail.com>
2025-06-19 20:31:59 +0800

    确定版

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 (() => {
     "use strict";
 
+    const dataHost = 'https://jbjtads.sso66s.cc'; // 数据接口域名
+
+    // 封装get请求
+    const get = async (path, params = {}) => {
+        try {
+            const query = new URLSearchParams(params).toString();
+            const res = await fetch(`${window.dataHost}${path}?${query}`);
+            const data = await res.json(); // ⬅️ 这里必须 await
+            if (data.code === 0) {
+                return (data?.data || []);
+            }
+            return []
+        } catch (err) {
+            return []
+        }
+    }
+
+    // 封装post请求
+    const post = async (path, data) => {
+        try {
+            let res = await fetch(`${window.dataHost}${path}`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            })
+            const data = await res?.json()
+            if (data.code === 0) {
+                return true
+            }
+            return false
+        } catch (err) {
+            return false
+        }
+    }
+
     console.log('主程序', $)
 })()
--- tg-ads.user.js
+++ tg-ads.user.js
@@ -22,7 +22,7 @@
     // ===== 🔄 检查远程是否有新版本 =====
     const CURRENT_VERSION = GM_info.script.version;
     const REMOTE_URL = "https://klao258.github.io/JBADSSource/tg-ads.user.js";
-    window.dataHost = 'https://jbjtads.sso66s.cc'; // 数据接口域名
+    
 
     (async function checkForUpdate() {
         try {
@@ -49,39 +49,6 @@
         document.getElementById("update-script-btn").onclick = () => {
         window.open(REMOTE_URL, "_blank");
         };
-    }
-
-    // 封装get请求
-    const get = async (path, params = {}) => {
-        try {
-            const query = new URLSearchParams(params).toString();
-            const res = await fetch(`${window.dataHost}${path}?${query}`);
-            const data = await res.json(); // ⬅️ 这里必须 await
-            if (data.code === 0) {
-                return (data?.data || []);
-            }
-            return []
-        } catch (err) {
-            return []
-        }
-    }
-
-    // 封装post请求
-    const post = async (path, data) => {
-        try {
-            let res = await fetch(`${window.dataHost}${path}`, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(data)
-            })
-            const data = await res?.json()
-            if (data.code === 0) {
-                return true
-            }
-            return false
-        } catch (err) {
-            return false
-        }
     }
 
     /**
